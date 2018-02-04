8960d3b47ccbdd4979135893e2a99d607473675d
tsuba5a <matthew.limjy@gmail.com>
2018-02-04 19:14:07 +0800

    Adding

---
+++ venv/Lib/site-packages/json_default.py
@@ -0,0 +1,32 @@
+
+'''
+Implementaion of the ideas from https://hynek.me/articles/serialization/
+'''
+import datetime
+from functools import singledispatch
+
+__version__ = '0.2'
+
+
+@singledispatch
+def default(obj):
+    '''Safe and sane default'''
+    return str(obj)
+
+
+@default.register(datetime.datetime)
+def _(obj):
+    r = obj.replace(microsecond=0).isoformat()
+    if r.endswith('+00:00'):
+        r = r[:-6] + 'Z'
+    return r
+
+
+@default.register(datetime.date)
+def _(obj):
+    return obj.isoformat()
+
+
+@default.register(datetime.time)
+def _(obj):
+    return obj.replace(microsecond=0).isoformat()
